1c1699cc5194970139a2a54269d72a27d4ca5b03
cappycap <bullara2@wwu.edu>
2019-11-30 09:42:38 -0800

    checkTime is organized into one file, checkTries, updates to login.json

--- access/checkTime.php
+++ access/checkTime.php
@@ -5,7 +5,20 @@ require 'db.php';
 // Define response array for delivering status.
 $response = array();
 
-$response['time'] = time();
+$time = time();
+
+if (isset($_POST['sub') {
+
+	$sub = -1 * ((int) $_POST['sub']);
+	$time += $sub;
+
+} else if (isset($_POST['add'])) {
+
+	$time += abs($_POST['add'];
+
+}
+
+$response['time'] = $time;
 
 echo json_encode($response);
 
